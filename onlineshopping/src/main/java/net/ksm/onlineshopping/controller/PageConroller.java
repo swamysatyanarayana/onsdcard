5e4cab587e6f6c982b10111765e28e6471f6a051
swamysatya <tswamy484@gmail.com>
2019-06-15 17:20:54 +0530

    successfully loaded static Resources

--- onlineshopping/src/main/java/net/ksm/onlineshopping/controller/PageConroller.java
+++ onlineshopping/src/main/java/net/ksm/onlineshopping/controller/PageConroller.java
@@ -1,7 +1,9 @@
 package net.ksm.onlineshopping.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -15,4 +17,20 @@ public class PageConroller {
 		return mv;
 	}
 	
+	/*@RequestMapping(value="test")
+	public ModelAndView test(@RequestParam(value="greeting", required=false) String greeting ) {
+		if(greeting == null)
+			greeting = "Welcome to Spring mvc";
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("welcome", greeting);
+		return mv;
+	}
+	@RequestMapping(value="test2/{greeting}")
+	public ModelAndView test2(@PathVariable("greeting") String greeting ) {
+		if(greeting == null)
+			greeting = "Welcome to Spring mvc";
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("welcome", greeting);
+		return mv;
+	}*/
 }
